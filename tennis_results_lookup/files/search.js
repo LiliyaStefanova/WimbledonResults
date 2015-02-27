40c08b59d41b209789321c908523762c4dbf8e45
LiliyaStefanova <liliya.stefanova@gmail.com>
2015-02-27 22:57:24 +0000

    created css outlined html file input section started functions in js file added gitignore

--- tennis_results_lookup/files/search.js
+++ tennis_results_lookup/files/search.js
@@ -1,3 +1,34 @@
 /**
  * Created by liliya on 26/02/2015.
  */
+
+function getXML(urlName){
+    var xhr = $.ajax({
+        url:urlName,
+        datatype:"xml",
+        async:false
+    })
+    return xhr.responseXML;
+    
+}
+
+function findResults(category,player,word_match,sets,pred,round,pred1,sort){
+    var styleSheet = getXML("results-style.xsl");
+    if(category.value=="men"){
+        var results = getXML("wimbledon-men-2013.xml");
+    }
+    else{
+        var results = getXML("wimbledon-women-2013.xml");
+    }
+   
+    if(typeof (XSLTProcessor) !== "undefined"){
+        var processor = new XSLTProcessor();
+        processor.importStylesheet(styleSheet);
+        var result = processor.transformToFragment(results, document);
+        document.getElementById("display").appendChild(result);}
+        else{
+        window.alert("Your browser does not support the XSLTProcessor object");
+        
+        player.getAttribute("value")
+    }
+}
