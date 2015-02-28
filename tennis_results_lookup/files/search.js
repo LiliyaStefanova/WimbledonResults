e0c2a984a85192e2e34d92914e1c7a1dfaad29ba
LiliyaStefanova <liliya.stefanova@gmail.com>
2015-02-28 22:35:09 +0000

    added jquery library updated results to have ids XSLTProcessor now loading and accessing nodes removed stylesheet from xml files

--- tennis_results_lookup/files/search.js
+++ tennis_results_lookup/files/search.js
@@ -7,28 +7,29 @@ function getXML(urlName){
         url:urlName,
         datatype:"xml",
         async:false
-    })
+    });
     return xhr.responseXML;
-    
 }
 
-function findResults(category,player,word_match,sets,pred,round,pred1,sort){
+function findResults(){
+
     var styleSheet = getXML("results-style.xsl");
-    if(category.value=="men"){
-        var results = getXML("wimbledon-men-2013.xml");
+    var results = null;
+    if($('#category')[0].value ==="men"){
+        results = getXML("wimbledon-men-2013.xml");
+    } else{
+        results = getXML("wimbledon-women-2013.xml");
     }
-    else{
-        var results = getXML("wimbledon-women-2013.xml");
-    }
-   
-    if(typeof (XSLTProcessor) !== "undefined"){
+    
+    if(typeof (XSLTProcessor) !== "undefined") {
         var processor = new XSLTProcessor();
         processor.importStylesheet(styleSheet);
-        var result = processor.transformToFragment(results, document);
-        document.getElementById("display").appendChild(result);}
-        else{
-        window.alert("Your browser does not support the XSLTProcessor object");
         
-        player.getAttribute("value")
+        
+        var result = processor.transformToFragment(results, document);
+        document.getElementById("display").appendChild(result)
+    } else {
+        window.alert("Your browser does not support the XSLTProcessor object");
     }
+
 }
